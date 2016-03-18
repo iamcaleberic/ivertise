b7b634a2ad78bd8db084618cf5d33b76729b192f
iamcaleberic <calebme@mail.com>
2016-03-18 08:55:32 +0000

    links

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,14 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+$('.eval.example .menu .item')
+  .tab({
+    evaluateScripts : 'once',
+    context         : 'parent',
+    auto            : true,
+    path            : '/'
+  })
+;
+$('.menu .item')
+  .tab()
+;
