a0a433836c2aa7ab47ff253e11e0a9e5701196bb
Saworieza <saworieza@gmail.com>
2016-03-22 17:18:19 +0300

    doing the cancancan dance

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 
+  #Authentication
+
 
   # Only allow letter, number, underscore and punctuation.
   validate :validate_username
